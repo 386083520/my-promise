708884d20c77a25d427db30e3d3194a8afd50b6f
386083520 <386083520@qq.com>
2021-10-27 20:15:25 +0800

    resolve和reject

--- src/core.js
+++ src/core.js
@@ -5,11 +5,23 @@ function Promise(fn) {
     doResolve(fn, this);
 }
 
+function resolve(self, newValue) {
+    self._state = 1;
+    self._value = newValue;
+}
+
+function reject(self, newValue) {
+    self._state = 2;
+    self._value = newValue;
+}
+
 function doResolve(fn, promise) {
     var res = tryCallTwo(fn, function (value) {
         console.log('gsdresolve', value)
+        resolve(promise, value);
     }, function (reason) {
         console.log('gsdreject', reason)
+        reject(promise, reason);
     })
 }
 
